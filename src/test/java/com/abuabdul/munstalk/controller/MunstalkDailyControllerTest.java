d67671154abbf9ddb19bb5861767379e71581ebd
abuabdul <abuabdul86@hotmail.com>
2015-12-23 01:12:38 +0530

    Adding mockito junit test

--- src/test/java/com/abuabdul/munstalk/controller/MunstalkDailyControllerTest.java
+++ src/test/java/com/abuabdul/munstalk/controller/MunstalkDailyControllerTest.java
@@ -1,9 +1,13 @@
 package com.abuabdul.munstalk.controller;
 
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
 /**
  * @author abuabdul
  *
  */
+@RunWith(MockitoJUnitRunner.class)
 public class MunstalkDailyControllerTest {
 
 }
